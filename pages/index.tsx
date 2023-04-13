5c8092964e7acdec5446cacae535647d161e3652
yfxstudios <yfxstudios@gmail.com>
2023-04-13 11:31:14 -0700

    Added input logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,48 @@
 import Head from "next/head";
 import { Roboto_Slab } from "next/font/google";
 import Keyboard from "@/components/Keyboard";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const RobotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
 export default function Home() {
-  const [key, setKey] = useState<string | null>(null);
-
   // useRef for pointerLocation
   const pointerLocation = useRef(0);
+  const pointerRow = useRef(1);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      setKey(e.key);
+      const letter = document.getElementsByClassName("letter");
+
+      // only allow user to type letters in the 5 letter row until enter is pressed and pointerRow is incremented
+      if (pointerRow.current < 6) {
+        if (e.key === "Backspace") {
+          // if backspace is pressed, move pointerLocation back one and remove the letter
+          if (pointerLocation.current > 0) {
+            pointerLocation.current--;
+            letter[pointerLocation.current].textContent = "";
+          }
+        } else if (e.key === "Enter") {
+          // if enter is pressed, move pointerRow down one and reset pointerLocation
+          if (pointerRow.current < 6) {
+            pointerRow.current++;
+            pointerLocation.current = 0;
+          }
+        } else {
+          // if any other key is pressed, add the letter to the current pointerLocation
+          if (pointerLocation.current < 5 && e.key.length === 1) {
+            letter[pointerLocation.current].textContent = e.key;
+            pointerLocation.current++;
+          }
+        }
+      }
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [key]);
-
-  useEffect(() => {
-    const letter = document.getElementsByClassName("letter");
-    if (key) {
-      if (letter[pointerLocation.current] && key.length === 1) {
-        letter[pointerLocation.current].textContent = key;
-        pointerLocation.current++;
-      }
-    }
-
-    if (key === "Backspace") {
-      if (letter[pointerLocation.current - 1]) {
-        letter[pointerLocation.current - 1].textContent = "";
-        pointerLocation.current--;
-      }
-    }
-  }, [key, pointerLocation]);
+  }, []);
 
   return (
     <>
@@ -51,7 +56,9 @@ export default function Home() {
         // use Roboto
         className={RobotoSlab.className}
       >
-        <h1>Wordle</h1>
+        <h1 className="text-4xl text-center font-bold text-gray-800 m-4">
+          Wordle
+        </h1>
         <div className="gameContainer">
           <div className="game">
             <div className="gameRow" id="1">
